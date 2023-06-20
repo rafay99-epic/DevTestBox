87ae59fa1f6d3807880e7fcc05fd83c243407368
Abdul Rafay <99marafay@gmail.com>
2023-06-21 02:23:57 +0500

    Fetch Data updating the UI

--- Firebase Database/Fetch Data/main.js
+++ Firebase Database/Fetch Data/main.js
@@ -10,388 +10,287 @@ const firebaseConfig = {
     measurementId: "G-FWKZLBB4F6"
 };
 
+
 firebase.initializeApp(firebaseConfig);
 
-
 // Get a reference to the Firestore database
-  const db = firebase.firestore();
-  
-  // Function to fetch and display data in the table
-  const fetchData = () => {
-    const tableBody = document.querySelector('tbody');
-    tableBody.innerHTML = ''; // Clear existing table data
-  
-    db.collection('assignments')
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((doc) => {
-          const data = doc.data();
-          const tableRow = document.createElement('tr');
-  
-          const fields = [
-            'assignmentFileURL',
-            'currentDate',
-            'deadline',
-            'domain',
-            'email',
-            'fullName',
-            'package',
-            'phone',
-            'specialInstructions'
-          ];
-  
-          fields.forEach((field) => {
-            const tableCell = document.createElement('td');
-            if (field === 'assignmentFileURL') {
-              const link = document.createElement('a');
-              link.href = data[field];
-              link.textContent = data[field];
-              tableCell.appendChild(link);
-            } else {
-              tableCell.textContent = data[field];
-            }
-            tableRow.appendChild(tableCell);
-          });
-  
-          const deleteCell = document.createElement('td');
-          const deleteIcon = document.createElement('i');
-          deleteIcon.classList.add('fas', 'fa-trash', 'delete-icon');
-          deleteCell.appendChild(deleteIcon);
-          tableRow.appendChild(deleteCell);
-  
-          tableBody.appendChild(tableRow);
-        });
-      })
-      .catch((error) => {
-        console.log('Error fetching data:', error);
-      });
-  };
-  
-  // Function to handle delete operation
-  // Function to handle delete operation
-const handleDelete = (docId) => {
-    db.collection('assignments')
-      .doc(docId)
-      .delete()
-      .then(() => {
-        alert('Record deleted successfully!');
-        fetchData(); // Refresh the table after deletion
-      })
-      .catch((error) => {
-        console.log('Error deleting record:', error);
-      });
-  };
-  
-  
-  // Event listener to handle delete icon click
-  document.addEventListener('click', (event) => {
-    if (event.target.classList.contains('delete-icon')) {
-      const tableRow = event.target.closest('tr');
-      const docId = tableRow.dataset.docId;
-      handleDelete(docId);
-    }
-  });
-  
-  // Fetch data on page load
-  document.addEventListener('DOMContentLoaded', fetchData);
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', fetchData);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const fetchData = () => {
-//     db.collection('assignments')
-//         .get()
-//         .then((snapshot) => {
-//             snapshot.forEach((doc) => {
-//                 const data = doc.data();
-//                 const tableRow = document.createElement('tr');
-
-//                 const fields = [
-//                     'assignmentFileURL',
-//                     'currentDate',
-//                     'deadline',
-//                     'domain',
-//                     'email',
-//                     'fullName',
-//                     'package',
-//                     'phone',
-//                     'specialInstructions'
-//                 ];
-
-//                 fields.forEach((field) => {
-//                     const tableCell = document.createElement('td');
-//                     tableCell.textContent = data[field];
-//                     tableRow.appendChild(tableCell);
-//                 });
-
-//                 const deleteCell = document.createElement('td');
-//                 const deleteIcon = document.createElement('i');
-//                 deleteIcon.classList.add('fas', 'fa-trash-alt', 'edit-icon');
-//                 deleteCell.appendChild(deleteIcon);
-//                 tableRow.appendChild(deleteCell);
-
-//                 document.querySelector('tbody').appendChild(tableRow);
-//             });
-//         })
-//         .catch((error) => {
-//             console.log('Error fetching data:', error);
-//         });
-// };
-
-
-
-
-
-
-
-// Working data with links and delete button
-
-// const fetchData = () => {
-//     db.collection('assignments')
-//         .get()
-//         .then((snapshot) => {
-//             snapshot.forEach((doc) => {
-//                 const data = doc.data();
-//                 const tableRow = document.createElement('tr');
-
-//                 const fields = [
-//                     'assignmentFileURL',
-//                     'currentDate',
-//                     'deadline',
-//                     'domain',
-//                     'email',
-//                     'fullName',
-//                     'package',
-//                     'phone',
-//                     'specialInstructions'
-//                 ];
-
-//                 fields.forEach((field) => {
-//                     const tableCell = document.createElement('td');
-//                     if (field === 'assignmentFileURL') {
-//                         const link = document.createElement('a');
-//                         link.href = data[field];
-//                         link.textContent = data[field];
-//                         tableCell.appendChild(link);
-//                     } else {
-//                         tableCell.textContent = data[field];
-//                     }
-//                     tableRow.appendChild(tableCell);
-//                 });
-
-//                 const editCell = document.createElement('td');
-//                 const deleteIcon = document.createElement('i');
-//                 deleteIcon.classList.add('fas', 'fa-trash-alt', 'edit-icon');
-//                 editCell.appendChild(deleteIcon);
-//                 tableRow.appendChild(editCell);
-
-//                 document.querySelector('tbody').appendChild(tableRow);
-//             });
-//         })
-//         .catch((error) => {
-//             console.log('Error fetching data:', error);
-//         });
-// };
-
-// ...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Working code with links
-// const fetchData = () => {
-//     db.collection('assignments')
-//       .get()
-//       .then((snapshot) => {
-//         snapshot.forEach((doc) => {
-//           const data = doc.data();
-//           const tableRow = document.createElement('tr');
-  
-//           const fields = [
-//             'assignmentFileURL',
-//             'currentDate',
-//             'deadline',
-//             'domain',
-//             'email',
-//             'fullName',
-//             'package',
-//             'phone',
-//             'specialInstructions'
-//           ];
-  
-//           fields.forEach((field) => {
-//             const tableCell = document.createElement('td');
-  
-//             if (field === 'assignmentFileURL') {
-//               const link = document.createElement('a');
-//               link.href = data[field];
-//               link.target = '_blank';
-//               link.textContent = data[field];
-//               tableCell.appendChild(link);
-//             } else {
-//               tableCell.textContent = data[field];
-//             }
-  
-//             tableRow.appendChild(tableCell);
-//           });
-  
-//           document.querySelector('tbody').appendChild(tableRow);
-//         });
-//       })
-//       .catch((error) => {
-//         console.log('Error fetching data:', error);
-//       });
-//   };
-  
-
-
-
-
-
-
-
-
-
-
-
-
-// Working Code
-// const fetchData = () => {
-//     db.collection('assignments')
-//       .get()
-//       .then((snapshot) => {
-//         snapshot.forEach((doc) => {
-//           const data = doc.data();
-//           const tableRow = document.createElement('tr');
-  
-//           const fields = [
-//             'assignmentFileURL',
-//             'currentDate',
-//             'deadline',
-//             'domain',
-//             'email',
-//             'fullName',
-//             'package',
-//             'phone',
-//             'specialInstructions'
-//           ];
-  
-//           fields.forEach((field) => {
-//             const tableCell = document.createElement('td');
+const db = firebase.firestore();
+
+// Function to fetch and display data in the table
+// const fetchData = (filters = {}) => {
+//   const tableBody = document.querySelector('tbody');
+//   tableBody.innerHTML = ''; // Clear existing table data
+
+//   let query = db.collection('assignments');
+
+//   // Apply filters
+//   if (filters.deadline) {
+//     query = query.where('deadline', '==', filters.deadline);
+//   }
+//   if (filters.package) {
+//     query = query.where('package', '==', filters.package);
+//   }
+
+//   query
+//     .get()
+//     .then((snapshot) => {
+//       snapshot.forEach((doc) => {
+//         const data = doc.data();
+//         const tableRow = document.createElement('tr');
+
+//         const fields = [
+//           'assignmentFileURL',
+//           'currentDate',
+//           'deadline',
+//           'domain',
+//           'email',
+//           'fullName',
+//           'package',
+//           'phone',
+//           'specialInstructions',
+//         ];
+
+//         fields.forEach((field) => {
+//           const tableCell = document.createElement('td');
+//           if (field === 'assignmentFileURL') {
+//             const link = document.createElement('a');
+//             link.href = data[field];
+//             link.textContent = data[field];
+//             tableCell.appendChild(link);
+//           } else {
 //             tableCell.textContent = data[field];
-//             tableRow.appendChild(tableCell);
-//           });
-  
-//           document.querySelector('tbody').appendChild(tableRow);
+//           }
+//           tableRow.appendChild(tableCell);
 //         });
-//       })
-//       .catch((error) => {
-//         console.log('Error fetching data:', error);
+
+//         tableBody.appendChild(tableRow);
 //       });
+//     })
+//     .catch((error) => {
+//       console.log('Error fetching data:', error);
+//     });
+// };
+
+
+// // Function to handle filter change
+// const handleFilterChange = () => {
+//   const deadline = document.getElementById('deadline-filter').value;
+//   const selectedPackage = document.getElementById('package-filter').value;
+
+//   const filters = {
+//     deadline: deadline || null,
+//     package: selectedPackage || null,
 //   };
-  
+
+//   fetchData(filters);
+// };
+
+// // Event listeners for filter changes
+// document.getElementById('deadline-filter').addEventListener('change', handleFilterChange);
+// document.getElementById('package-filter').addEventListener('change', handleFilterChange);
+
+// // Fetch data on page load
+// document.addEventListener('DOMContentLoaded', fetchData);
+
+
+
+
+
+// Function to fetch and display data in the table
+// const fetchData = (filters = {}, searchTerm = '') => {
+//   const tableBody = document.querySelector('tbody');
+//   tableBody.innerHTML = ''; // Clear existing table data
+
+//   let query = db.collection('assignments');
+
+//   // Apply filters
+//   if (filters.deadline) {
+//     query = query.where('deadline', '==', filters.deadline);
+//   }
+//   if (filters.package) {
+//     query = query.where('package', '==', filters.package);
+//   }
+
+//   query
+//     .get()
+//     .then((snapshot) => {
+//       const filteredData = [];
+//       snapshot.forEach((doc) => {
+//         const data = doc.data();
+//         filteredData.push(data);
+//       });
+
+//       // Apply search term
+//       const searchData = searchTerm.trim().toLowerCase();
+//       const filteredResults = filteredData.filter((item) => {
+//         const email = item.email.toLowerCase();
+//         const fullName = item.fullName.toLowerCase();
+//         return email.includes(searchData) || fullName.includes(searchData);
+//       });
+
+//       filteredResults.forEach((item) => {
+//         const tableRow = document.createElement('tr');
+
+//         const fields = [
+//           'assignmentFileURL',
+//           'currentDate',
+//           'deadline',
+//           'domain',
+//           'email',
+//           'fullName',
+//           'package',
+//           'phone',
+//           'specialInstructions',
+//         ];
+
+//         fields.forEach((field) => {
+//           const tableCell = document.createElement('td');
+//           if (field === 'assignmentFileURL') {
+//             const link = document.createElement('a');
+//             link.href = item[field];
+//             link.textContent = item[field];
+//             tableCell.appendChild(link);
+//           } else {
+//             tableCell.textContent = item[field];
+//           }
+//           tableRow.appendChild(tableCell);
+//         });
+
+//         tableBody.appendChild(tableRow);
+//       });
+//     })
+//     .catch((error) => {
+//       console.log('Error fetching data:', error);
+//     });
+// };
+
+// // Function to handle filter change
+// const handleFilterChange = () => {
+//   const deadline = document.getElementById('deadline-filter').value;
+//   const selectedPackage = document.getElementById('package-filter').value;
+
+//   const filters = {
+//     deadline: deadline || null,
+//     package: selectedPackage || null,
+//   };
+
+//   fetchData(filters);
+// };
+
+// // Event listeners for filter changes
+// document.getElementById('deadline-filter').addEventListener('change', handleFilterChange);
+// document.getElementById('package-filter').addEventListener('change', handleFilterChange);
+
+// // Function to handle search input change
+// const handleSearchChange = () => {
+//   const searchInput = document.getElementById('search-input').value;
+//   fetchData({}, searchInput);
+// };
+
+// // Event listener for search input change
+// document.getElementById('search-input').addEventListener('input', handleSearchChange);
+
+// // Fetch data on page load
+// document.addEventListener('DOMContentLoaded', fetchData);
+
+
+
+// Function to fetch and display data in the table
+const fetchData = (filters = {}, searchTerm = '') => {
+  const tableBody = document.querySelector('tbody');
+  tableBody.innerHTML = ''; // Clear existing table data
+
+  let query = db.collection('assignments');
+
+  // Apply filters
+  if (filters.deadline) {
+    const deadlineDate = new Date(filters.deadline);
+    const nextDay = new Date(deadlineDate);
+    nextDay.setDate(deadlineDate.getDate() + 1);
+    query = query.where('deadline', '>=', deadlineDate).where('deadline', '<', nextDay);
+  }
+  if (filters.package) {
+    query = query.where('package', '==', filters.package);
+  }
+
+  query
+    .get()
+    .then((snapshot) => {
+      const filteredData = [];
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+        filteredData.push(data);
+      });
+
+      // Apply search term
+      const searchData = searchTerm.trim().toLowerCase();
+      const filteredResults = filteredData.filter((item) => {
+        const email = item.email.toLowerCase();
+        const fullName = item.fullName.toLowerCase();
+        return email.includes(searchData) || fullName.includes(searchData);
+      });
+
+      filteredResults.forEach((item) => {
+        const tableRow = document.createElement('tr');
+
+        const fields = [
+          'assignmentFileURL',
+          'currentDate',
+          'deadline',
+          'domain',
+          'email',
+          'fullName',
+          'package',
+          'phone',
+          'specialInstructions',
+        ];
+
+        fields.forEach((field) => {
+          const tableCell = document.createElement('td');
+          if (field === 'assignmentFileURL') {
+            const link = document.createElement('a');
+            link.href = item[field];
+            link.textContent = item[field];
+            tableCell.appendChild(link);
+          } else {
+            tableCell.textContent = item[field];
+          }
+          tableRow.appendChild(tableCell);
+        });
+
+        tableBody.appendChild(tableRow);
+      });
+    })
+    .catch((error) => {
+      console.log('Error fetching data:', error);
+    });
+};
+
+// Function to handle filter change
+const handleFilterChange = () => {
+  const deadline = document.getElementById('deadline-filter').value;
+  const selectedPackage = document.getElementById('package-filter').value;
+
+  const filters = {
+    deadline: deadline || null,
+    package: selectedPackage || null,
+  };
+
+  fetchData(filters);
+};
+
+// Event listeners for filter changes
+document.getElementById('deadline-filter').addEventListener('change', handleFilterChange);
+document.getElementById('package-filter').addEventListener('change', handleFilterChange);
+
+// Function to handle search input change
+const handleSearchChange = () => {
+  const searchInput = document.getElementById('search-input').value;
+  fetchData({}, searchInput);
+};
+
+// Event listener for search input change
+document.getElementById('search-input').addEventListener('input', handleSearchChange);
+
+// Fetch data on page load
 document.addEventListener('DOMContentLoaded', fetchData);
